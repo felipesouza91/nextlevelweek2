01fd21c407c2d6e6a9e3523baea8199752330ef3
Felipe Souza <felipedb91@gmail.com>
2020-08-06 23:31:09 -0300

    refactor: move input button html to a Component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
+import Input from '../../components/Input';
 
 const TeacherForm: React.FC = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrivel que voce quer dar aulas" />
+      <PageHeader 
+        title="Que incrivel que voce quer dar aulas"
+        description="O primeiro passo e preencher esse formulário de inscrição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seu dados</legend>
+          <Input type="text" label="Nome Completo" name="name" />
+          <Input type="text" label="Avatar" name="avatar" />
+          <Input type="text" label="Whatsapp" name="whatsapp" />
+        </fieldset>
+      </main>
     </div>
   );
 };
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -5,24 +5,16 @@ import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 const TeacherList: React.FC = () => {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponiveis">
         <form action="" id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Materia</label>
-            <input type="text" id="subject" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input type="text" label="Materia" name="subject"/>
+          <Input type="text" label="Dia da semana" name="week-day"/>
+          <Input type="text" label="Hora" name="time"/>
         </form>
       </PageHeader>
 
